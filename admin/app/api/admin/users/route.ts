777ec75578657272f8571b98173d7aa08f04801a
belaidaymen444 <belaidaymen2003@gmail.com>
2025-10-17 16:54:31 +0100

    `Remove import statement from bcrypt and import it directly`

--- admin/app/api/admin/users/route.ts
+++ admin/app/api/admin/users/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@/lib/generated/prisma'
-import * as bcrypt from "bcrypt"
+import  bcrypt from "bcrypt"
 
 const prisma = new PrismaClient()
 
